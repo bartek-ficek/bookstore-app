116ac252075bc0b16c48325536b90eab80a0d62f
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-28 21:42:09 +0100

    created method configureGlobal() method in SecurityConfig class

--- src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
+++ src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.bartekficek.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,5 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe();
     }
 
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+    }
 }

ee1c771f95b4e0c35d1970a0ac16b30f6e1e5cb3
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-28 21:35:46 +0100

    Overrode method configure() in SecurityConfig class

--- src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
+++ src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserSecurityService userSecurityService;
@@ -27,8 +29,25 @@ public class SecurityConfig {
             "/myAccount"
     };
 
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO 6:15
+        http
+                .authorizeRequests()
+                /*.antMatchers("/**")*/
+                .antMatchers(PUBLIC_MATCHERS)
+                .permitAll().anyRequest().authenticated();
+
+        http
+                .csrf().disable().cors().disable()
+                .formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+                .and()
+                .rememberMe();
     }
+
 
 }

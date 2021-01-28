ef2eb173840b602d45d9cec7abc3ddecd0096a67
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-28 21:48:16 +0100

    created class UserSecurityService

--- src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
+++ src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe();
     }
 
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
     }

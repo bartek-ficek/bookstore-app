cce41031e544f63417d6d5989de152e31aec1fe8
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-30 05:30:23 +0100

    created UserService interface

---
+++ src/main/java/pl/bartekficek/bookstore/service/impl/UserService.java
@@ -0,0 +1,11 @@
+package pl.bartekficek.bookstore.service.impl;
+
+import pl.bartekficek.bookstore.domain.User;
+import pl.bartekficek.bookstore.domain.security.PasswordResetToken;
+
+public interface UserService {
+
+    PasswordResetToken getPasswordResetToken(final String token);
+
+    void createPasswordResetTokenForUser(final User user, final String token);
+}

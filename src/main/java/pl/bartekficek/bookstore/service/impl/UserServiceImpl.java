501b28de6d57d77c022f44c01440dac9193e5f30
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-30 06:16:31 +0100

    Autowired PasswordResetTokenRepository to UserServiceImpl

--- src/main/java/pl/bartekficek/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/pl/bartekficek/bookstore/service/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package pl.bartekficek.bookstore.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.bartekficek.bookstore.domain.User;
 import pl.bartekficek.bookstore.domain.security.PasswordResetToken;
+import pl.bartekficek.bookstore.repository.PasswordResetTokenRepository;
 import pl.bartekficek.bookstore.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    PasswordResetTokenRepository passwordResetTokenRepository;
 
     @Override
     public PasswordResetToken getPasswordResetToken(final String token) {

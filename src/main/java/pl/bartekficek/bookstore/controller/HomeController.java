2c28144effe4c4866492003033c4609b660b48dd
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-01-30 21:39:51 +0100

    userService field autowired to HomeContoller

--- src/main/java/pl/bartekficek/bookstore/controller/HomeController.java
+++ src/main/java/pl/bartekficek/bookstore/controller/HomeController.java
@@ -1,15 +1,20 @@
 package pl.bartekficek.bookstore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.bartekficek.bookstore.domain.security.PasswordResetToken;
+import pl.bartekficek.bookstore.service.UserService;
 
 import java.util.Locale;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/login")
     public String login(Model model) {
@@ -23,14 +28,14 @@ public class HomeController {
     }
 
     @RequestMapping("/createAccount")
-    public String createNewAccount(Model model) {
+    public String createNewAccount(Locale locale, @RequestParam ("token") String token, Model model) {
+        PasswordResetToken passToken = userService.getPasswordToken(token);
         model.addAttribute("classActiveNewAccount", true);
         return "myAccount";
     }
 
     @RequestMapping("/forgetPassword")
-    public String forgetPassword(Locale locale, @RequestParam ("token") String token, Model model) {
-        PasswordResetToken passToken = userService.getPasswordToken(token);
+    public String forgetPassword(Model model) {
         model.addAttribute("classActiveForgetPassword", true);
         return "myAccount";
     }

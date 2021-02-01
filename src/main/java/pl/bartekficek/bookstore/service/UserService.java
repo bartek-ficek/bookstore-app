9f6d27900a7a546d898e091fd0c5de0dfea34652
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-01 15:45:28 +0100

    created method createUser() in UserService interface

--- src/main/java/pl/bartekficek/bookstore/service/UserService.java
+++ src/main/java/pl/bartekficek/bookstore/service/UserService.java
@@ -16,5 +16,5 @@ public interface UserService {
 
     User findByEmail(String email);
 
-    User createUser(User user, Set<UserRole> userRoles);
+    User createUser(User user, Set<UserRole> userRoles) throws Exception;
 }

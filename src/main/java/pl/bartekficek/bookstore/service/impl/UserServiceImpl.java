dbbd6692753489ec8bf12252020a8af64140df6e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-01 15:44:36 +0100

    implemented method createUser() in UserServiceImpl

--- src/main/java/pl/bartekficek/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/pl/bartekficek/bookstore/service/impl/UserServiceImpl.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.bartekficek.bookstore.domain.User;
 import pl.bartekficek.bookstore.domain.security.PasswordResetToken;
+import pl.bartekficek.bookstore.domain.security.UserRole;
 import pl.bartekficek.bookstore.repository.PasswordResetTokenRepository;
+import pl.bartekficek.bookstore.repository.RoleRepository;
 import pl.bartekficek.bookstore.repository.UserRepository;
 import pl.bartekficek.bookstore.service.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,6 +21,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public PasswordResetToken getPasswordResetToken(final String token) {
@@ -36,5 +44,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+        User localUser = userRepository.findByUsername(user.getUsername());
+        if (localUser != null) {
+            throw new Exception("User already exists. Nothing will be done");
+        } else {
+            for (UserRole ur : userRoles) {
+                roleRepository.save(ur.getRole());
+            }
+            user.getUserRoles().addAll(userRoles);
+
+            localUser = userRepository.save(user);
+        }
+        return localUser;
     }
 }

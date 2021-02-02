6871f10a3441adb9d0c00853f4bb106c513e12f6
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-02 10:43:20 +0100

    implementation of CommandLineRunner in starting class BookstoreApplication

--- src/main/java/pl/bartekficek/bookstore/BookstoreApplication.java
+++ src/main/java/pl/bartekficek/bookstore/BookstoreApplication.java
@@ -1,13 +1,22 @@
 package pl.bartekficek.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Implementation of CommandLineRunner interface and run() method added here to create default user.
+ * Very convenient to testing already ready functionalities.
+ */
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }

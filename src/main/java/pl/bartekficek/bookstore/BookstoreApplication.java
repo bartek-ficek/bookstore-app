9828f9a42aa951242fa2f3d0a6c30c411c67f0a4
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-02 10:54:15 +0100

    created default user1 in run() method (BookstoreApplication class)

--- src/main/java/pl/bartekficek/bookstore/BookstoreApplication.java
+++ src/main/java/pl/bartekficek/bookstore/BookstoreApplication.java
@@ -1,8 +1,17 @@
 package pl.bartekficek.bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.bartekficek.bookstore.domain.User;
+import pl.bartekficek.bookstore.domain.security.Role;
+import pl.bartekficek.bookstore.domain.security.UserRole;
+import pl.bartekficek.bookstore.service.UserService;
+import pl.bartekficek.bookstore.utility.SecurityUtility;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Implementation of CommandLineRunner interface and run() method added here to create default user.
@@ -11,12 +20,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookstoreApplication implements CommandLineRunner {
 
+    @Autowired
+    UserService userService;
+
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        User user1 = new User();
+        user1.setFirstName("Jan");
+        user1.setLastName("Kowalski");
+        user1.setUsername("JK");
+        user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+        user1.setEmail("JKowalski@gmail.com");
+        Set<UserRole> userRoles = new HashSet<>();
+        Role role1 = new Role();
+        role1.setRoleId(1);
+        role1.setName("ROLE_USER");
+        userRoles.add(new UserRole(user1,role1));
 
+        userService.createUser(user1, userRoles);
     }
 }

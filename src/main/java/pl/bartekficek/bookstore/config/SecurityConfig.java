d67bd8378e0809f55dfb3b8742a5ac8296e3f3b6
Bartek Ficek <ficek.bartlomiej@gmail.com>
2021-02-03 08:45:56 +0100

    new Paths added in PUBLIC_MATCHERS field (SecurityConfig)

--- src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
+++ src/main/java/pl/bartekficek/bookstore/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/js/**",
             "/image/**",
             "/",
-            "/myAccount"
+            "/newUser",
+            "/forgetPassword"
     };
 
 
